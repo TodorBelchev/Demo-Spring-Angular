24dffeb9f7d3b1cb7291d09445b003ae2800999b
Todor Belchev <todorbelchev@live.com>
2023-01-29 09:56:37 +0200

    Init server

---
+++ server/src/main/java/com/belchev/examples/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.belchev.examples.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
